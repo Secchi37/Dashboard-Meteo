13a90e74cfe74d5d4c7c91b12dbec966d83ccf87
Secchi37 <secchibarbara85@gmail.com>
2023-02-27 15:56:52 +0100

    upgrade navbar stily meny

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 //import logo from './logo.svg';
 import "./App.css";
+import React from "react";
 import HomePage from "./pages/HomePage";
 import Dashboard from "./pages/Dashboard";
 import NavBar from "./components/NavBar";
 import { Routes, Route } from "react-router-dom";
 
+
+
 function App() {
+ 
   return (
-    <div>
+    <div fluid className="App">    
       <NavBar />
       <Routes>
         <Route path="/" element={<HomePage />} />
--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -5,25 +5,16 @@ import { markers } from "../DATA/objectData";
 import "../App.css";
 
 export default function Dashboard(props){
-const olbia= [40.9237, 9.4963];
-const villasor=[39.3804, 8.9409];
-const oristano=[39.9056, 8.5923];
-const sassari=[40.7293, 8.5497];
-  
 
 
-    return(
-    <div>
-    Dashboard page  
-    <div className="mappa"><div className="qMaps"><Maps markers={markers} position={position}/></div>
-    <div className="qMaps"><Maps position={olbia}/></div>
-    <div className="qMaps"><Maps position={sassari}/></div>
-    <div className="qMaps"><Maps position={villasor}/></div>
-    <div className="qMaps"><Maps position={oristano}/></div></div> 
-    
-   
-
-    
-    </div>
-    )
+    return (
+      <div>
+        Dashboard page
+        <div className="mappa">
+          <div className="qMaps">
+            <Maps markers={markers} position={position} />
+          </div>          
+        </div>
+      </div>
+    );
 }
