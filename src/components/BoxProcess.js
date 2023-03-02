e16e34bc35bab3ae003cc79ec6a7ec96ca547dcf
Secchi37 <secchibarbara85@gmail.com>
2023-03-02 23:15:14 +0100

    update BoxProcess

--- src/components/BoxProcess.js
+++ src/components/BoxProcess.js
@@ -1,13 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import { Badge, Card, CardGroup, Container, Row } from "react-bootstrap";
 import { datiP } from "../DATA/DataProcess";
 import '../components/styles.css'
 
 
-export default function BoxProcess(){
-   // const[view,setView]=useState(false);
 
-    console.log(`${datiP[0].img}`);
+export default function BoxProcess(){
+    const[view,setView]=useState('light');
+    const[num,setNum]=useState(null); 
+
+  
     return(
         <Container fluid style={{ marginTop: 15 }}>
             <CardGroup style={{ margintTop: 50 }}>
@@ -17,12 +19,28 @@ export default function BoxProcess(){
                         left: 0, position: 'relative', zIndex: 1, 
                     }}>
                         <Row style={{ justifyContent: 'right',marginTop:10}} >
-                            <Badge border='dark' bg='' text="dark" className="point"
-                                style={{ width: 25, height: 25, borderColor: 'black', borderRadius: 50, marginRight: '35%',top:'80%'}}>{datiP.id}</Badge></Row>
-                        <Container className="imgMovie"
+                        
+                        <Badge bg={num===index? view:'ligth'} text="dark" key={index} pill
+                        style={{border:'1px solid rgba(0, 0, 0, 0.25)',borderWidth:'100%', width: 25, height: 25, borderRadius: '80%', marginRight: '35%',top:'80%'}}
+                        >{datiP.id}</Badge>
+                            
+                                </Row>
+                        <Container className="imgMovie" key={index}
+                        onMouseEnter={() => {  
+                            console.log('dato',datiP.id[0]-1);
+                            console.log('indice',index+1);
+
+                           setView('warning');
+                           setNum(datiP.id[0]-1)
+                          }}
+                          onMouseLeave={() => {                            
+                            setView('ligth');
+                            setNum(null)        
+                                                   
+                         }}
                         //style={{borderRadius:50,boxShadow:' 0px 5px 20px 10px rgba(0, 0, 0, 0.10)'}}
                          >
-                        <Card.Img src={require('../img/icons/' + `${datiP.img}` + '.png')} className='colorText'
+                        <Card.Img src={require('../img/icons/' + `${datiP.img}` + '.png')} className={num===index? 'colorText1':'colorText'}
                             style={{
                                 marginTop:'25%',
                                 width: 63, heigth: 63,
